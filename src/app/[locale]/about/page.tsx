59106c5427afc63680f9af44d37b902ec6b78e20
Lucas de Castro <lucascastrofr@gmail.com>
2025-03-15 01:32:00 -0300

    refactor: change about section

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -4,13 +4,15 @@ export default function About() {
   const t = useTranslations("About");
 
   return (
-    <div className="flex flex-col gap-2 justify-center font-[family-name:var(--font-geist-sans)] flex-full">
+    <section className="flex flex-col gap-2 font-[family-name:var(--font-geist-sans)] flex-full">
       <h1 className="text-6xl text-gray-200">{t("title")}</h1>
-      <p className="text-xl max-w-prose">{t("p1")}</p>
-      <p className="text-xl max-w-prose">{t("p2")}</p>
-      <p className="text-xl max-w-prose">{t("p3")}</p>
-      <p className="text-xl max-w-prose">{t("p4")}</p>
-      <p className="text-xl max-w-prose">{t("ending-message")}</p>
-    </div>
+      <article className="flex flex-col gap-2">
+        <p className="text-xl max-w-prose">{t("p1")}</p>
+        <p className="text-xl max-w-prose">{t("p2")}</p>
+        <p className="text-xl max-w-prose">{t("p3")}</p>
+        <p className="text-xl max-w-prose">{t("p4")}</p>
+        <p className="text-xl max-w-prose">{t("ending-message")}</p>
+      </article>
+    </section>
   );
 }
