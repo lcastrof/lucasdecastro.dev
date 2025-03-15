8ce2fe8d283fa0651487b2378e247430da3574d6
Lucas de Castro <lucascastrofr@gmail.com>
2025-03-15 00:53:22 -0300

    feat: add copy for about page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,16 @@
+import { useTranslations } from "next-intl";
+
 export default function About() {
+  const t = useTranslations("About");
+
   return (
     <div className="flex flex-col gap-2 justify-center font-[family-name:var(--font-geist-sans)] flex-full">
-      <h1 className="text-6xl text-gray-200">About</h1>
-      <p className="text-xl max-w-prose">
-        This is a simple blog built with Next.js and Tailwind CSS. You can find
-        the source code somewhere, I guess.
-      </p>
+      <h1 className="text-6xl text-gray-200">{t("title")}</h1>
+      <p className="text-xl max-w-prose">{t("p1")}</p>
+      <p className="text-xl max-w-prose">{t("p2")}</p>
+      <p className="text-xl max-w-prose">{t("p3")}</p>
+      <p className="text-xl max-w-prose">{t("p4")}</p>
+      <p className="text-xl max-w-prose">{t("ending-message")}</p>
     </div>
   );
 }
