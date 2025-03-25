39a58864db54b722e7721eb0822f9c3779cb607b
Lucas de Castro <lucascastrofr@gmail.com>
2025-03-25 00:09:02 -0300

    fix(blogpost): add notFound page when not finding post

--- src/app/[locale]/blog/[slug]/page.tsx
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getBlogPostsSlugs, getPost } from "@/lib/blog";
 import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: Promise<{ locale: string; slug: string }>;
@@ -7,41 +8,47 @@ type Props = {
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
   const { locale, slug } = await params;
-  const post = await getPost(slug);
 
-  return {
-    title: post.title,
-    description: post.description,
-    alternates: {
-      canonical: `/${locale}/blog/${slug}`,
-      languages: {
-        en: `/en/blog/${slug}`,
-        pt: `/pt/blog/${slug}`,
-      },
-    },
-    openGraph: {
+  try {
+    const post = await getPost(slug);
+
+    return {
       title: post.title,
       description: post.description,
-      type: "article",
-      publishedTime: post.date,
-      authors: ["Lucas de Castro"],
-      url: `https://lucasdecastro.dev/${locale}/blog/${slug}`,
-      locale: locale === "en" ? "en_US" : "pt_BR",
-      siteName: "Lucas de Castro",
-      images: [
-        {
-          url: `https://lucasdecastro.dev/og?type=blog&title=${encodeURIComponent(
-            post.title || ""
-          )}`,
-          width: 1200,
-          height: 600,
+      alternates: {
+        canonical: `/${locale}/blog/${slug}`,
+        languages: {
+          en: `/en/blog/${slug}`,
+          pt: `/pt/blog/${slug}`,
         },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-    },
-  };
+      },
+      openGraph: {
+        title: post.title,
+        description: post.description,
+        type: "article",
+        publishedTime: post.date,
+        authors: ["Lucas de Castro"],
+        url: `https://lucasdecastro.dev/${locale}/blog/${slug}`,
+        locale: locale === "en" ? "en_US" : "pt_BR",
+        siteName: "Lucas de Castro",
+        images: [
+          {
+            url: `https://lucasdecastro.dev/og?type=blog&title=${encodeURIComponent(
+              post.title || ""
+            )}`,
+            width: 1200,
+            height: 600,
+          },
+        ],
+      },
+      twitter: {
+        card: "summary_large_image",
+      },
+    };
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    notFound();
+  }
 }
 
 export default async function PostPage({
@@ -50,9 +57,14 @@ export default async function PostPage({
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await params;
-  const { default: Post } = await import(`@/posts/${slug}.mdx`);
 
-  return <Post />;
+  try {
+    const { default: Post } = await import(`@/posts/${slug}.mdx`);
+    return <Post />;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    notFound();
+  }
 }
 
 export function generateStaticParams() {
